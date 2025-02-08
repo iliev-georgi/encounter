7a6db8d279c22615c574a09d219a18fe9ac672b3
Georgi Iliev <georgi.iliev@tailkeep.com>
2025-02-08 16:08:22 +0200

    Select encounter location on map

--- options/annotate.py
+++ options/annotate.py
@@ -7,6 +7,9 @@ from config import *
 from helper import join_labels
 from helper import register_encounter
 from pixelfed_functions import get_statuses, get_attached_media
+from model import Location
+import folium
+from streamlit_folium import st_folium
 
 
 def render_annotate(user_info):
@@ -14,46 +17,58 @@ def render_annotate(user_info):
     to_annotate_list = get_statuses(user_info["id"], st.session_state["token"]["access_token"])
 
     for to_annotate in to_annotate_list:
+        if "last_location" not in st.session_state:
+            st.session_state.last_location = ((Location.latitude, Location.longitude))
         column1, column2 = st.columns([1, 2])
         with column1:
             attached_media = get_attached_media(to_annotate.preview_url, st.session_state["token"]["access_token"])
             st.image(BytesIO(attached_media), caption=to_annotate.content)
         with column2:
             with st.expander("Annotate"):
-                name = st_keyup("Enter bird name", key=to_annotate.id)
-                if name and len(name) > 3:
-                    lookup_table = get_filtered_list(name)
-                    append_previews_to(lookup_table)
-                    lookup_table = join_labels(lookup_table)
-                else:
-                    lookup_table = dict()
-                for suggestion in lookup_table:
-                    column1, column2 = st.columns([1, 2])
-                    with column1:
-                        try:
-                            if (
-                                suggestion.thumbnail
-                                and requests.get(
-                                    suggestion.thumbnail,
-                                    timeout=3,
-                                    allow_redirects=True,
-                                ).status_code
-                                != 404
-                            ):
-                                st.image(suggestion.thumbnail)
-                            else:
-                                st.markdown("**No preview found**")
-                        except:
-                            st.markdown("__Preview error__")
-                    with column2:
-                        st.button(
-                            suggestion.prefLabel,
-                            on_click=register_encounter,
-                            help=suggestion.abstract,
-                            args=[
-                                to_annotate.time,
-                                user_info.get("url"),
-                                to_annotate.preview_url,
-                                suggestion.species
-                            ],
-                        )
+                search_tab, pin_tab = st.tabs([":mag: Search species", ":round_pushpin: Pin location"])
+                map = folium.Map()
+                with search_tab:    
+                    name = st_keyup("Enter bird name", key=to_annotate.id)
+                    if name and len(name) > 3:
+                        lookup_table = get_filtered_list(name)
+                        append_previews_to(lookup_table)
+                        lookup_table = join_labels(lookup_table)
+                    else:
+                        lookup_table = dict()
+                    for suggestion in lookup_table:
+                        column1, column2 = st.columns([1, 2])
+                        with column1:
+                            try:
+                                if (
+                                    suggestion.thumbnail
+                                    and requests.get(
+                                        suggestion.thumbnail,
+                                        timeout=3,
+                                        allow_redirects=True,
+                                    ).status_code
+                                    != 404
+                                ):
+                                    st.image(suggestion.thumbnail)
+                                else:
+                                    st.markdown("**No preview found**")
+                            except:
+                                st.markdown("__Preview error__")
+                        with column2:
+                            st.button(
+                                suggestion.prefLabel,
+                                on_click=register_encounter,
+                                help=suggestion.abstract,
+                                args=[
+                                    to_annotate.time,
+                                    user_info.get("url"),
+                                    to_annotate.preview_url,
+                                    suggestion.species,
+                                    st.session_state.last_location[0],
+                                    st.session_state.last_location[1],
+                                ],
+                            )
+                with pin_tab:
+                    map.add_child(folium.LatLngPopup())
+                    st_data = st_folium(map)
+                    if st_data.get("last_clicked"):
+                        st.session_state.last_location = ((st_data["last_clicked"]["lat"], st_data["last_clicked"]["lng"]))
