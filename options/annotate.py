7ae84a5dffe85627bb7b34f9a43f648c7cb0a092
Georgi Iliev <georgi.iliev@tailkeep.com>
2025-02-16 09:55:35 +0200

    Separate annotate, explore logic into streamlit fragments

--- options/annotate.py
+++ options/annotate.py
@@ -11,9 +11,113 @@ from config import *
 from helper import join_labels
 from helper import register_encounter
 from pixelfed_functions import get_statuses, get_attached_media
-from model import Location
+from model import Location, ToAnnotate
 import folium
 from streamlit_folium import st_folium
+
+
+def update_plot_and_register_encounter(
+    to_annotate: ToAnnotate,
+    marker_label: str,
+    user: str,
+    species: str,
+    latitude: float,
+    longitude: float,
+):
+    
+    # update location and label on map
+    to_annotate.label = marker_label
+    to_annotate.lat = latitude
+    to_annotate.lng = longitude
+
+    # persist encounter
+    register_encounter(
+        input_id=to_annotate.id,
+        time=to_annotate.time,
+        user=user,
+        evidence=to_annotate.preview_url,
+        species=species,
+        latitude=latitude,
+        longitude=longitude,
+    )
+
+
+@st.fragment
+def plot_encounter_location(to_annotate):
+    center_x, center_y = Location.latitude, Location.longitude
+    map = folium.Map()
+    map.add_child(folium.LatLngPopup())
+    if all(
+        mandatory is not None
+        for mandatory in [
+            to_annotate.lat,
+            to_annotate.lng,
+            to_annotate.label,
+        ]
+    ):
+        map.add_child(
+            folium.Marker(
+                location=(to_annotate.lat, to_annotate.lng),
+                icon=folium.Icon(color="darkblue", icon="crow", prefix="fa"),
+                tooltip=to_annotate.label,
+            )
+        )
+        center_x, center_y = to_annotate.lat, to_annotate.lng
+        st.session_state.last_location[to_annotate.id] = (center_x, center_y)
+
+    st_data = st_folium(map, center=(center_x, center_y), zoom=10, key=to_annotate.id)
+    if st_data.get("last_clicked"):
+        st.session_state.last_location[to_annotate.id] = (
+            st_data["last_clicked"]["lat"],
+            st_data["last_clicked"]["lng"],
+        )
+        to_annotate.lat = st_data["last_clicked"]["lat"]
+        to_annotate.lng = st_data["last_clicked"]["lng"]
+        if not to_annotate.label:
+            to_annotate.label = "Unknown"
+
+
+@st.fragment
+def suggest_species(to_annotate, user_info):
+    name = st_keyup("Enter bird name", key=to_annotate.id, debounce=500)
+    if name and len(name) > 3:
+        lookup_table = get_filtered_list(name)
+        append_previews_to(lookup_table)
+        lookup_table = join_labels(lookup_table)
+    else:
+        lookup_table = dict()
+    for suggestion in lookup_table:
+        column1, column2 = st.columns([1, 2])
+        with column1:
+            try:
+                if (
+                    suggestion.thumbnail
+                    and requests.get(
+                        suggestion.thumbnail,
+                        timeout=3,
+                        allow_redirects=True,
+                    ).status_code
+                    != 404
+                ):
+                    st.image(suggestion.thumbnail)
+                else:
+                    st.markdown("**No preview found**")
+            except:
+                st.markdown("__Preview error__")
+        with column2:
+            st.button(
+                suggestion.prefLabel,
+                on_click=update_plot_and_register_encounter,
+                help=suggestion.abstract,
+                kwargs=dict(
+                    to_annotate=to_annotate,
+                    marker_label=suggestion.prefLabel,
+                    user=user_info.get("url"),
+                    species=suggestion.species,
+                    latitude=st.session_state.last_location[to_annotate.id][0],
+                    longitude=st.session_state.last_location[to_annotate.id][1],
+                ),
+            )
 
 
 def render_annotate(user_info):
@@ -26,7 +130,10 @@ def render_annotate(user_info):
     for to_annotate in to_annotate_list:
         if "last_location" not in st.session_state:
             st.session_state.last_location = dict()
-            st.session_state.last_location[to_annotate.id] = (Location.latitude, Location.longitude)
+        st.session_state.last_location[to_annotate.id] = (
+            Location.latitude,
+            Location.longitude,
+        )
         column1, column2 = st.columns([1, 2])
         with column1:
             attached_media = get_attached_media(
@@ -42,73 +149,7 @@ def render_annotate(user_info):
                     [":mag: Search species", ":round_pushpin: Pin location"]
                 )
                 with search_tab:
-                    name = st_keyup("Enter bird name", key=to_annotate.id)
-                    if name and len(name) > 3:
-                        lookup_table = get_filtered_list(name)
-                        append_previews_to(lookup_table)
-                        lookup_table = join_labels(lookup_table)
-                    else:
-                        lookup_table = dict()
-                    for suggestion in lookup_table:
-                        column1, column2 = st.columns([1, 2])
-                        with column1:
-                            try:
-                                if (
-                                    suggestion.thumbnail
-                                    and requests.get(
-                                        suggestion.thumbnail,
-                                        timeout=3,
-                                        allow_redirects=True,
-                                    ).status_code
-                                    != 404
-                                ):
-                                    st.image(suggestion.thumbnail)
-                                else:
-                                    st.markdown("**No preview found**")
-                            except:
-                                st.markdown("__Preview error__")
-                        with column2:
-                            st.button(
-                                suggestion.prefLabel,
-                                on_click=register_encounter,
-                                help=suggestion.abstract,
-                                args=[
-                                    to_annotate.time,
-                                    user_info.get("url"),
-                                    to_annotate.preview_url,
-                                    suggestion.species,
-                                    st.session_state.last_location[to_annotate.id][0],
-                                    st.session_state.last_location[to_annotate.id][1],
-                                ],
-                            )
-                with pin_tab:
-                    center_x, center_y = Location.latitude, Location.longitude
-                    map = folium.Map()
-                    map.add_child(folium.LatLngPopup())
-                    if all(
-                        mandatory is not None
-                        for mandatory in [
-                            to_annotate.lat,
-                            to_annotate.lng,
-                            to_annotate.label,
-                        ]
-                    ):
-                        map.add_child(
-                            folium.Marker(
-                                location=(to_annotate.lat, to_annotate.lng),
-                                icon=folium.Icon(
-                                    color="darkblue", icon="crow", prefix="fa"
-                                ),
-                                tooltip=to_annotate.label,
-                            )
-                        )
-                        center_x, center_y = to_annotate.lat, to_annotate.lng
+                    suggest_species(to_annotate=to_annotate, user_info=user_info)
 
-                    st_data = st_folium(
-                        map, center=(center_x, center_y), zoom=10
-                    )
-                    if st_data.get("last_clicked"):
-                        st.session_state.last_location[to_annotate.id] = (
-                            st_data["last_clicked"]["lat"],
-                            st_data["last_clicked"]["lng"],
-                        )
+                with pin_tab:
+                    plot_encounter_location(to_annotate=to_annotate)
